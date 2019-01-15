4ed0bb7aa0e3b90c698c9672ae1dff38484a5298
helpthx <jvrbaptista@live.com>
2019-01-15 11:00:13 -0200

    [UP] New topic 13 with Notebook 13

---
+++ GTx: CSE6040x: FA18: Computing for Data Analysis/Module 2: The analysis of data/Topic 13: Classification (logistic regression)/Part_1.py
@@ -0,0 +1,4 @@
+'''
+Exercise 3 (2 points). 
+Implement the logistic function. Inspect the resulting plot of  G(y)G(y)  in 1-D and then the contour plot of  G(θTx)G(θTx) . Your function should accept a Numpy matrix of values, Y, and apply the sigmoid elementwise.
+'''
