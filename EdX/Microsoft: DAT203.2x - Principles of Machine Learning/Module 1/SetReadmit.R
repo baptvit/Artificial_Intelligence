e554b4464eba7779eef04a4e9c6028bc32241d60
helpthx <jvrbaptista@live.com>
2019-02-13 19:48:06 -0200

    [DEL] Dirs

--- EdX/Microsoft: DAT203.2x - Principles of Machine Learning/Module 1/SetReadmit.R
+++
@@ -1,9 +0,0 @@
-class.readmit <- function(x){
-  out <- rep("NO", length(x))
-  out[which(x != "NO")] <- "YES"
-  out
-}
-
-df <- maml.mapInputPort(1)
-df$readmitted <- class.readmit(df$readmitted)
-maml.mapOutputPort('df')
